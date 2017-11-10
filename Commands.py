3bbbc0f30181a03230d8908800e215e3b8ca3421
SteelPh0enix <phoenixpl512@gmail.com>
2017-11-10 11:15:08 +0100

    Almost complete refactor of GCCHelper

--- Commands.py
+++ Commands.py
@@ -3,48 +3,44 @@ import os
 import subprocess
 import sys
 
-import GCCHelper
+from GCCHelper import GCCHelper
+
+GCCRUNNER = GCCHelper()
 
 
-class Commands:
-    """Class containing functionality of bot"""
+def info(args):
+    """Gets info about script"""
+    if args is not None:
+        docstring = getattr(sys.modules[__name__], str(args)).__doc__
+        if docstring is not None:
+            return docstring
+    return """SelFBot v0.1 by SteelPh0enix\nPowered by Python {} on {}""".format(
+        sys.version, sys.platform)
 
-    @staticmethod
-    def info(args):
-        """Gets info about script"""
-        if args is not None:
-            docstring = getattr(Commands, str(args)).__doc__
-            if docstring is not None:
-                return docstring
-        return """SelFBot v0.1 by SteelPh0enix\nPowered by Python {} on {}""".format(
-            sys.version, sys.platform)
 
-    @staticmethod
-    def runc(args):
-        """Runs C code snippet and returns the human-readable verbose output"""
-        data = GCCHelper.run_c_code(args)
+def runc(code):
+    """Runs C code snippet and returns the human-readable verbose output"""
+    data = GCCRUNNER.run_c_code(code)
 
-    @staticmethod
-    def runcxx(args):
-        """Runs C++ code snippet and returns the human-readable verbose output"""
-        pass
 
-    @staticmethod
-    def runfc(args):
-        """Runs C code as whole file.
- Returns the human-readable verbose output"""
-        pass
+def runcxx(code):
+    """Runs C++ code snippet and returns the human-readable verbose output"""
+    pass
 
-    @staticmethod
-    def runfcxx(args):
-        """Runs C++ code as whole file.
- Returns the human-readable verbose output"""
-        pass
 
-    @staticmethod
-    def restart(args):
-        """Restarts the bot in mode sent by arguments"""
-        if args is None or args == "":
-            return "You have to enter restart mode!"
-        subprocess.run(["python", os.path.join(
-            os.path.split(os.path.realpath(__file__))[0], "Restart.py"), args, str(os.getpid())])
+def runfc(code):
+    """Runs C code as whole file. Returns the human-readable verbose output"""
+    pass
+
+
+def runfcxx(code):
+    """Runs C++ code as whole file. Returns the human-readable verbose output"""
+    pass
+
+
+def restart(arg):
+    """Restarts the bot in mode sent by arguments"""
+    if not arg:
+        return "You have to enter restart mode!"
+    subprocess.run(["python", os.path.join(
+        os.path.split(os.path.realpath(__file__))[0], "Restart.py"), arg, str(os.getpid())])
--- GCCHelper.py
+++ GCCHelper.py
@@ -4,121 +4,132 @@ import subprocess
 
 import requests
 
-C_COMPILER = "gcc"
-CXX_COMPILER = "g++"
 
-C_FLAGS = ["-Wall", "-Wextra", "-Wpedantic", "-std=c11"]
-CXX_FLAGS = ["-Wall", "-Wextra", "-Wpedantic", "-std=c++1z"]
+class GCCHelper:
+    """GCC execution helper class"""
 
-FILE_PATH = os.path.join(os.path.split(
-    os.path.realpath(__file__))[0], "tmp")
-if not os.path.exists(FILE_PATH):
-    os.makedirs(FILE_PATH)
-C_FILE_PATH = os.path.join(FILE_PATH, "tmp.c")
-CXX_FILE_PATH = os.path.join(FILE_PATH, "tmp.cpp")
-OUTPUT_FILE = os.path.join(FILE_PATH, "tmp.exe")
+    def __init__(self):
+        self.C_COMPILER = "gcc"
+        self.CXX_COMPILER = "g++"
 
-C_HEADERS = ["stdio.h", "time.h", "stdlib.h", "string.h"]
-CXX_HEADERS = ["iostream", "array", "vector",
-               "algorithm", "string", "chrono", "random"]
+        self.C_FLAGS = ["-Wall", "-Wextra", "-Wpedantic", "-std=c11"]
+        self.CXX_FLAGS = ["-Wall", "-Wextra", "-Wpedantic", "-std=c++1z"]
 
+        self.FILE_PATH = os.path.join(os.path.split(
+            os.path.realpath(__file__))[0], "tmp")
+        if not os.path.exists(self.FILE_PATH):
+            os.makedirs(self.FILE_PATH)
+            self.C_FILE_PATH = os.path.join(self.FILE_PATH, "tmp.c")
+            self.CXX_FILE_PATH = os.path.join(self.FILE_PATH, "tmp.cpp")
+        self.OUTPUT_FILE = os.path.join(self.FILE_PATH, "tmp.exe")
 
-def run_c_code(code):
-    """Runs C code in main() function"""
-    # Prepare the boilerplate code
-    runcode = "// Generated using GCCHelper by SteelPh0enix\n"
-    for header in C_HEADERS:
-        runcode += "#include <" + header + ">\n"
+        self.C_HEADERS = ["stdio.h", "time.h", "stdlib.h", "string.h"]
+        self.CXX_HEADERS = ["iostream", "array", "vector",
+                            "algorithm", "string", "chrono", "random"]
 
-    runcode += "\nint main() {\n" + code + "\n}\n"
-    return compile_and_run_c(runcode)
+        self.C_HEADERS_CODE = '\n'.join(["#include <{}>".format(x) for x in self.C_HEADERS])
+        self.CXX_HEADERS_CODE = '\n'.join(["#include <{}>".format(x) for x in self.CXX_HEADERS])
 
+        self.CODE_COMMENT = "// Generated using GCCHelper by SteelPh0enix\n"
 
-def run_cxx_code(code):
-    """Runs C++ code in main() function"""
-    runcode = "// Generated using GCCHelper by SteelPh0enix\n"
-    for header in CXX_HEADERS:
-        runcode += "#include <" + header + ">\n"
+    def run_c_code(self, code: str) -> dict:
+        """Runs C code in main() function
+        :rtype: dict
+        :param code: Code to execute
+        :return: look: compile_and_run_c"""
 
-    runcode += "\nint main() {\n" + code + "\n}\n"
-    return compile_and_run_cxx(runcode)
+        runcode = self.CODE_COMMENT + self.C_HEADERS_CODE + "\nint main() {\n{0}\n}\n".format(code)
+        return self.compile_and_run_c(runcode)
 
+    def run_cxx_code(self, code: str) -> dict:
+        """Runs C++ code in main() function
+        :rtype: dict
+        :param code: Code to execute
+        :return: look: compile_and_run_cxx"""
 
-def compile_and_run_c(code):
-    """Executes the C code. Returns dict with keys 'compilation',
-    and (if compilation was successful) 'execution'.
-    Both keys contain keys 'stdout', 'stderr' and 'retcode'."""
-    # print("Code: {}\n".format(code))
+        runcode = self.CODE_COMMENT + self.CXX_HEADERS_CODE + "\nint main() {\n{0}\n}\n".format(code)
+        return self.compile_and_run_cxx(runcode)
 
-    with open(C_FILE_PATH, mode="w") as c_file:
-        c_file.write(code)
+    def compile_and_run_c(self, code: str) -> dict:
+        """Executes the C code.
+        :rtype: dict
+        :param code: Code to execute (full file)
+        :return: dict with objects 'compilation', and (if compilation was successful) 'execution'.
+        Both objects contain keys 'stdout', 'stderr' and 'retcode'."""
 
-    comp_stdout, comp_stderr, comp_code = run_and_get_output(
-        [C_COMPILER] + C_FLAGS + ["-o", OUTPUT_FILE, C_FILE_PATH])
+        with open(self.C_FILE_PATH, mode="w") as c_file:
+            c_file.write(code)
 
-    ret_dict = {
-        'compilation':
-        {'stdout': comp_stdout, 'stderr': comp_stderr, 'retcode': comp_code}
-    }
+        comp_stdout, comp_stderr, comp_code = run_and_get_output(
+            [self.C_COMPILER] + self.C_FLAGS + ["-o", self.OUTPUT_FILE, self.C_FILE_PATH])
 
-    if comp_code != 0:
+        ret_dict = {
+            'compilation':
+                {'stdout': comp_stdout, 'stderr': comp_stderr, 'retcode': comp_code}
+        }
+
+        if not comp_code:  # there was an error during compilation - return code != 0
+            return ret_dict
+
+        exec_stdout, exec_stderr, exec_code = run_and_get_output(
+            [self.OUTPUT_FILE])
+
+        ret_dict['execution'] = {
+            'stdout': exec_stdout, 'stderr': exec_stderr, 'retcode': exec_code
+        }
+
         return ret_dict
 
-    exec_stdout, exec_stderr, exec_code = run_and_get_output(
-        [OUTPUT_FILE])
+    def compile_and_run_cxx(self, code: str) -> dict:
+        """Executes the C++ code.
+        :rtype: dict
+        :param code: Code to execute (full file)
+        :return: dict with objects 'compilation', and (if compilation was successful) 'execution'.
+        Both objects contain keys 'stdout', 'stderr' and 'retcode'."""
 
-    ret_dict['execution'] = {
-        'stdout': exec_stdout, 'stderr': exec_stderr, 'retcode': exec_code
-    }
+        with open(self.CXX_FILE_PATH, mode="w") as cxx_file:
+            cxx_file.write(code)
 
-    return ret_dict
+        comp_stdout, comp_stderr, comp_code = run_and_get_output(
+            [self.CXX_COMPILER] + self.CXX_FLAGS + ["-o", self.OUTPUT_FILE, self.CXX_FILE_PATH])
 
+        ret_dict = {
+            'compilation':
+                {'stdout': comp_stdout, 'stderr': comp_stderr, 'retcode': comp_code}
+        }
 
-def compile_and_run_cxx(code):
-    """Executes the C++ code. Returns dict with keys 'compilation',
-    and (if compilation was successful) 'execution'.
-    Both keys contain keys 'stdout', 'stderr' and 'retcode'."""
-    # print("Code: {}\n".format(code))
+        if not comp_code:  # there was an error during compilation - return code != 0
+            return ret_dict
 
-    with open(CXX_FILE_PATH, mode="w") as cxx_file:
-        cxx_file.write(code)
+        exec_stdout, exec_stderr, exec_code = run_and_get_output(
+            [self.OUTPUT_FILE])
 
-    comp_stdout, comp_stderr, comp_code = run_and_get_output(
-        [CXX_COMPILER] + CXX_FLAGS + ["-o", OUTPUT_FILE, CXX_FILE_PATH])
+        ret_dict['execution'] = {
+            'stdout': exec_stdout, 'stderr': exec_stderr, 'retcode': exec_code
+        }
 
-    ret_dict = {
-        'compilation':
-        {'stdout': comp_stdout, 'stderr': comp_stderr, 'retcode': comp_code}
-    }
-
-    if comp_code != 0:
         return ret_dict
 
-    exec_stdout, exec_stderr, exec_code = run_and_get_output(
-        [OUTPUT_FILE])
 
-    ret_dict['execution'] = {
-        'stdout': exec_stdout, 'stderr': exec_stderr, 'retcode': exec_code
-    }
-
-    return ret_dict
-
-
-def run_and_get_output(arguments):
-    "Runs the program and returns (stdout, stderr, return_code) tuple"
+def run_and_get_output(arguments: list) -> tuple:
+    """Runs the program and returns (stdout, stderr, return_code) tuple
+    :rtype: tuple(str, str, int)
+    :param arguments: list with executable path and it's arguments
+    :return: Tuple with stdout and stderr content, and return code of program
+    """
     proc = subprocess.Popen(
         arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate()
-    # print("Arguments: {}\nProc: {}\n Out: {}\n Err: {}\n Return code: {}\n".format(
-    #     arguments, proc, out, err, proc.returncode
-    # ))
     return out.decode('utf-8'), err.decode('utf-8'), proc.returncode
 
 
-def post_on_hastebin(content):
+def post_on_hastebin(content: str) -> str:
     """Helper function.
     Posts the stuff on Hastebin, returns URL.
     Yea, this is copy&paste from hastebin.py
-    https://github.com/LyricLy/hastebin.py/blob/master/hastebin/hastebin.py"""
+    https://github.com/LyricLy/hastebin.py/blob/master/hastebin/hastebin.py
+    :rtype: str
+    :param content: Text to be put on Hastebin
+    :return: Link to the paste"""
     post = requests.post("https://hastebin.com/documents", data=content)
-    return "https://hastebin.com/" + post.json()["key"]
+    return str("https://hastebin.com/" + post.json()["key"])
